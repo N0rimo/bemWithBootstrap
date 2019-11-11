e0c63474559573f7d53a7d7eef7730466856e93a
N0rimo <norimo@protonmain.com>
2019-11-11 07:49:14 +0200

    Update Portfolio section

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@ $(document).ready(function() {
 			if($(this).attr('val') == 'off') {
 				$('.button[filter]').attr('val', 'off');
 				$(this).attr('val', 'on');
+				$('.button[filter]').removeClass('focused');
+				$(this).addClass('focused');
 				$('.filter > div').show(300);
 			}
 		} else if ($(this).attr('val') == 'off') {
 			$('.button[filter]').attr('val', 'off');
 			$(this).attr('val', 'on');
+			$('.button[filter]').removeClass('focused');
+			$(this).addClass('focused');
 			$('.filter > div').hide(300);
 			var filter = $(this).attr('filter');
 			$('.filter > div[filter='+filter+']').show(300);

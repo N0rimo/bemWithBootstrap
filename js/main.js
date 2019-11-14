3aeed35e62426cb32597183f727ba3220bff3703
N0rimo <norimo@protonmain.com>
2019-11-14 12:49:12 +0200

    Add Team section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-	$('.button[filter]').click(function() {
+$(document).ready(function () {
+	$('.button[filter]').click(function () {
 		if ($(this).attr('filter') == 'all') {
-			if($(this).attr('val') == 'off') {
+			if ($(this).attr('val') == 'off') {
 				$('.button[filter]').attr('val', 'off');
 				$(this).attr('val', 'on');
 				$('.button[filter]').removeClass('focused');
@@ -15,7 +15,15 @@ $(document).ready(function() {
 			$(this).addClass('focused');
 			$('.filter > div').hide(300);
 			var filter = $(this).attr('filter');
-			$('.filter > div[filter='+filter+']').show(300);
+			$('.filter > div[filter=' + filter + ']').show(300);
 		}
+	});
+	$('.multiple-items').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		arrows: false,
+		dots: true,
+		dotsClass: 'dots-style',
 	});
 });
